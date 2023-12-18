d7c96cf31c05d53d5de804af480fcb003ba16ea4
Pakker <gfabinam@gmail.com>
2023-12-18 18:50:26 +0300

    1.1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+void PrintNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        PrintNumbers(m + 1, n);
+    }
+}
+
+Console.WriteLine($"Введите диапазон натуральных числел");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Натуральные числа от {M} до {N}:");
+PrintNumbers(M, N);
