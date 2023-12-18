205101ab1c9ec4283b4e56a0820a8526525f6827
Pakker <gfabinam@gmail.com>
2023-12-18 19:25:58 +0300

    3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+int[] FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr) Console.Write(item + " ");
+    Console.WriteLine();
+}
+
+void PrintArrayReversed(int[] arr, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write($"{arr[index]} ");
+        PrintArrayReversed(arr, index - 1);
+    }
+}
+
+int[] array = new int[10];
+array = FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArrayReversed(array, array.Length - 1);
+
